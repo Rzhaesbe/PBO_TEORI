73798a566e14bd9f39f4d44b37fb74ebcdc81a5a
Rasis al zailany hasibuan <121251679+Rzhaesbe@users.noreply.github.com>
2023-05-15 20:20:16 +0700

    Add files via upload

---
+++ PBO2201081011/src/main/java/com/mycompany/pbo2201081011/BukuAlamat.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pbo2201081011;
+
+/**
+ *
+ * @author Uncle.haesbe
+ */
+public class BukuAlamat {
+    
+}
---
+++ PBO2201081011/src/main/java/com/mycompany/pbo2201081011/PBO2201081011.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pbo2201081011;
+
+/**
+ *
+ * @author Uncle.haesbe
+ */
+public class PBO2201081011 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
